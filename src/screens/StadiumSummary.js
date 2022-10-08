d002320ede5b02aa3b46ab44f2cd1c6e699de3f6
Ali <ali@MacBook-Pro-Ali.local>
2022-10-08 10:04:50 +0300

    Stadum reports Functional development, binding to API

--- src/screens/StadiumSummary.js
+++ src/screens/StadiumSummary.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import {
   StyleSheet,
   View,
@@ -11,199 +11,268 @@ import {
   ImageBackground,
 } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
-import { Header, StatusBar, Text, BlockTitle, ListItem, TeamListItem, Button } from '@components'
+import { Header, StatusBar, Text, BlockTitle, ListItem, TeamListItem, Button, AvarageItem, AverageBlock } from '@components'
 import { mainApi } from '@api';
 import { loaderAction } from '@redux/actions/loaderActions'
 import MultiSlider from '@ptomasroos/react-native-multi-slider'
+import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
+import moment from 'moment';
 
 
-export default function About({ route, navigation }) {
+export default function PlayerSummary({ route, navigation }) {
   // const dispatch = useDispatch()
-  const [page, setPage] = useState({})
-  // useEffect(async () => {
-  //   try {
-  //     dispatch(loaderAction({ isLoading: true }))
-  //     const page = await mainApi.getPage({ id: 1065 });
-  //     setPage(page);
-  //     dispatch(loaderAction({ isLoading: false }))
-  //   } catch (e) {
-  //     dispatch(loaderAction({ isLoading: false }))
-  //   }
-  // }, []);
-  const { title = '', content = '' } = page;
+  const item = route?.params?.item
+  const refCarousel = useRef();
+  const refCarousel2 = useRef();
+  const [summary, setSummary] = useState([])
+  const [summaryMeta, setSummaryMeta] = useState([])
+  const [reports, setReports] = useState([])
+  const [activeTab, setActiveTab] = useState('summary')
+  const [avarageType, setAvarageType] = useState('avg')
+  const avarageTypes = ['avg', 'median', 'percentile_75', 'percentile_90']
+
+  useEffect(() => {
+    getActivityStadium()
+    getStadiumReports()
+  }, []);
+
+  const getActivityStadium = async () => {
+    try {
+      const res = await mainApi.getStadiumSummary(item.id)
+      console.log('res.data.data', res.data.data)
+      setSummary(res.data.data)
+      let summaryMeta = []
+      Object.keys(res.data.meta).forEach((key) => {
+        summaryMeta.push({
+          key,
+          value: res.data.meta[key]
+        })
+      });
+      setSummaryMeta(summaryMeta)
+    } catch (e) {
+      console.log('e', e)
+    }
+  }
+
+  const getStadiumReports = async () => {
+    try {
+      const res = await mainApi.getStadiumReports(item.id)
+      console.log('resres', res.data.data)
+      setReports(res.data.data)
+    } catch (e) {
+      console.log('e', e)
+    }
+  }
+
+  const summaryOptions = {
+    average: "Average",
+    median: "Median",
+    percentile_75: "75 Percentile",
+    percentile_90: "95 Percentile",
+    total_reviews: "Total Reviews"
+  }
+
+
+  const renderReports = () => {
+    return (
+      <View style={{ paddingHorizontal: 20 }}>
+        <FlatList
+          data={reports}
+          keyExtractor={(item, index) => 'report-' + index}
+          renderItem={({ item }) => (
+            <ListItem
+              value={parseFloat(item?.avg).toFixed(2)}
+              amount={item?.event?.user?.full_name}
+              title={`${item?.event?.sport?.name} • ${moment(item?.posted).format('DD MMM YYYY')}`}
+              icon={{
+                uri: item?.thumbnail?.url
+              }}
+              disabled
+            // onPress={() => navigation.navigate('PlayerReport')}
+            />
+          )}
+          ListFooterComponent={<View style={{ height: 100 }} />}
+        />
+      </View>
+    )
+  }
+
+
+  const renderSummary = () => {
+    return (
+      <View style={{ paddingHorizontal: 20 }}>
+
+
+        <View style={styles.wrapCarousel2}>
+
+          <TouchableOpacity
+            onPress={() => {
+              refCarousel2.current?.scrollTo({ count: 1, animated: true });
+            }}
+          >
+            <Image source={require('@assets/icons/cicle-arrow-left.png')} style={styles.arrowIcon} />
+          </TouchableOpacity>
+
+
+
+          <Carousel
+            loop
+            width={300}
+            height={110}
+            ref={refCarousel2}
+            data={summaryMeta}
+            scrollAnimationDuration={1000}
+            // onSnapToItem={(index) => setAvarageType(avarageTypes[index])}
+            renderItem={({ item, index }) => (
+              <AverageBlock
+                key={'ab-' + index}
+                title={summaryOptions[item.key]}
+                value={item.value}
+              />
+            )}
+          />
+
+
+          <TouchableOpacity
+            onPress={() => {
+              refCarousel2.current?.scrollTo({ count: -1, animated: true });
+            }}
+          >
+            <Image source={require('@assets/icons/cicle-arrow-right.png')} style={styles.arrowIcon} />
+          </TouchableOpacity>
+
+
+        </View>
+
+
+
+
+
+
+
+
+        <View style={styles.carousel}>
+
+          <TouchableOpacity
+            onPress={() => {
+              refCarousel.current?.scrollTo({ count: 1, animated: true });
+            }}
+          >
+            <Image source={require('@assets/icons/arrow-left.png')} style={styles.arrowIcon2} />
+          </TouchableOpacity>
+
+          <Carousel
+            loop
+            width={280}
+            height={20}
+            ref={refCarousel}
+            data={avarageTypes}
+            scrollAnimationDuration={1000}
+            onSnapToItem={(index) => setAvarageType(avarageTypes[index])}
+            renderItem={({ index }) => (
+              <View key={'a-' + index} style={styles.avarageShowWrap}>
+                <Text style={styles.avarageShowText}>SHOW: <Text style={{ color: '#5FC422' }}>{avarageTypes[index]} stats</Text></Text>
+              </View>
+            )}
+          />
+
+          <TouchableOpacity
+            onPress={() => {
+              refCarousel.current?.scrollTo({ count: -1, animated: true });
+            }}
+          >
+            <Image source={require('@assets/icons/arrow-right.png')} style={styles.arrowIcon2} />
+          </TouchableOpacity>
+        </View>
+
+
+        {summary?.map((i, key) => {
+          const value = parseFloat(i[avarageType])?.toFixed(1)
+          return (
+            <AvarageItem
+              key={'s-' + key}
+              title={i?.title}
+              value={value}
+              range={value?.replace('.', '')}
+            />
+          )
+        })}
+
+
+
+
+
+        {/* <Button
+          leftComponent={<Image source={require('@assets/icons/shape.png')} style={{ width: 26, height: 29, marginRight: 8 }} />}
+          text='Issue A New Scouting Report'
+          style={{ width: '100%', marginTop: 36, marginBottom: 20 }}
+          textStyle={{ fontFamily: 'Oswald' }}
+          onPress={() => navigation.navigate('PlayerEdit')}
+        /> */}
+
+      </View>
+    )
+  }
+
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle='dark-content' />
       <Header
-        title={title}
         navigation={navigation}
         goBack={navigation.goBack}
       />
       <ScrollView>
+
+
         <View
-          style={{ height: 315, width: '100%' }}
+          style={styles.bodyHeader}
         >
           <Image
-            style={{ height: 315, width: '100%' }}
-            source={{ uri: 'https://besthqwallpapers.com/Uploads/2-3-2019/82386/thumb2-red-bull-arena-4k-mls-empty-stadium-soccer.jpg' }}
+            style={styles.image}
+            source={{ uri: item?.thumbnail?.url }}
           />
-          <View style={{ height: 130, width: '100%', backgroundColor: 'rgba(0,0,0,0.5)', position: 'absolute', bottom: 0, paddingLeft: 14, paddingTop: 15, flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20 }}>
+          <View style={styles.nameWrap}>
             <View>
-              <Text style={{ fontWeight: '700', fontSize: 32, color: '#FFF' }}>Red Bull Arena</Text>
-
+              <Text style={styles.name} numberOfLines={1}>
+                {item?.name}
+              </Text>
               <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <Image source={require('@assets/icons/ball.png')} style={{ width: 16, height: 16 }} />
-                <Text style={{ fontFamily: 'Oswald', fontWeight: '500', fontSize: 12, color: '#FFF', marginLeft: 5, textTransform: 'uppercase' }}>soccer • Harrison, NJ, USA</Text>
+                <Text style={{ fontFamily: 'Oswald', fontWeight: '500', fontSize: 12, color: '#FFF', marginLeft: 5, textTransform: 'uppercase' }}>
+                  {item.city}, {item.country}
+                </Text>
               </View>
             </View>
             <View style={{ height: 140, top: -80 }}>
-              <Image source={require('@assets/images/337-3379384_logo-tottenham-hotspurs-tottenham-hotspurs-hd-png-download.png')} style={{ width: 42, height: 42, borderRadius: 21 }} />
-              <Image source={require('@assets/images/icons8-england.png')} style={{ width: 42, height: 42, borderRadius: 21, marginTop: 9 }} />
-              <Text style={{ fontFamily: 'Oswald', fontSize: 32, fontWeight: '700', marginTop: 7, color: '#FFFFFF' }}>9.2</Text>
+              <Image source={{ uri: item?.team?.thumbnail?.url }} style={styles.logo1} />
+              <Image source={{ uri: item?.national_team?.thumbnail?.url }} style={styles.logo2} />
+              <Text style={styles.avarageValue}>
+                {summaryMeta?.find(i => i.key === 'average')?.value}
+              </Text>
             </View>
           </View>
         </View>
 
 
 
-        <View style={{
-          top: -32,
-          height: 64, borderRadius: 32, backgroundColor: '#FFFF', width: '100%', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 0,
-          },
-          shadowOpacity: 0.3,
-          shadowRadius: 3.22,
-          elevation: 3,
-        }}>
-          <TouchableOpacity>
-            <Text style={{ color: '#5EC422', fontFamily: 'Oswald', fontWeight: '700', fontSize: 20, fontFamily: 'Oswald', textTransform: 'uppercase' }}>Summary</Text>
+        <View style={styles.tabs}>
+          <TouchableOpacity onPress={() => setActiveTab('summary')}>
+            <Text style={activeTab === 'summary' ? styles.activeTab : styles.tab}>Summary</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => navigation.navigate('StadiumDetail')}>
-            <Text style={{ color: '#7D86A9', fontFamily: 'Oswald', fontWeight: '700', fontSize: 20, fontFamily: 'Oswald', textTransform: 'uppercase' }}>Detail Reports</Text>
+          <TouchableOpacity onPress={() => setActiveTab('reports')}>
+            <Text style={activeTab === 'reports' ? styles.activeTab : styles.tab}>Detail Reports</Text>
           </TouchableOpacity>
         </View>
 
 
 
-        <View style={{ paddingHorizontal: 20 }}>
-
-          <View style={{
-            shadowOffset: {
-              width: 0,
-              height: 0,
-            },
-            shadowOpacity: 0.2,
-            shadowRadius: 3.22,
-            elevation: 3,
-            width: 220,
-            height: 98,
-            backgroundColor: '#fff',
-            borderRadius: 7
-          }}>
-            <View style={{ width: '100%', height: 35, backgroundColor: '#00293B', alignItems: 'center', justifyContent: 'center', borderTopRightRadius: 7, borderTopLeftRadius: 7 }}>
-              <Text style={{ fontFamily: 'Oswald', fontWeight: '700', fontSize: 12, color: '#FFFFFF' }}>Avarage Stats total</Text>
-            </View>
-
-            <View style={{ width: '100%', height: 62, justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={{ fontWeight: '400', fontSize: 42, color: '#000000' }}>9.2</Text>
-            </View>
-          </View>
 
 
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 34, marginBottom: 26 }}>
-            <Image source={require('@assets/icons/arrow-left.png')} style={{ width: 6.72, height: 11.26 }} />
-            <Text style={{ fontFamily: 'Oswald', fontweight: '700', fontSize: 16, textTransform: 'uppercase', color: '#00293B' }}>SHOW: <Text style={{ color: '#5FC422' }}>average stats</Text></Text>
-            <Image source={require('@assets/icons/arrow-right.png')} style={{ width: 6.72, height: 11.26 }} />
-          </View>
+        {activeTab === 'summary' && renderSummary()}
 
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Power Moves</Text>
-              <Text style={styles.optionValue}>9.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '10%' }} />
-            </ImageBackground>
-          </View>
-
-
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Finesse Moves</Text>
-              <Text style={styles.optionValue}>4.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '60%' }} />
-            </ImageBackground>
-          </View>
-
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Acceleration</Text>
-              <Text style={styles.optionValue}>7.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '30%' }} />
-            </ImageBackground>
-          </View>
-
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Block Shedding</Text>
-              <Text style={styles.optionValue}>10.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '0%' }} />
-            </ImageBackground>
-          </View>
-
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Tackling</Text>
-              <Text style={styles.optionValue}>2.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '80%' }} />
-            </ImageBackground>
-          </View>
-
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Penetration</Text>
-              <Text style={styles.optionValue}>5.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '50%' }} />
-            </ImageBackground>
-          </View>
-
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Motor</Text>
-              <Text style={styles.optionValue}>9.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '10%' }} />
-            </ImageBackground>
-          </View>
-
-          <View>
-            <View style={styles.textWrap}>
-              <Text style={styles.optionTitle}>Speed</Text>
-              <Text style={styles.optionValue}>6.0</Text>
-            </View>
-            <ImageBackground source={require('@assets/images/Rectangle.png')} style={styles.gradientLine}>
-              <View style={{ ...styles.grayLine, width: '40%' }} />
-            </ImageBackground>
-          </View>
-
-
-        </View>
-
-
+        {activeTab === 'reports' && renderReports()}
 
 
 
@@ -221,10 +290,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-  sliderTitle: { fontWeight: '800', color: '#2B2B2B', fontSize: 18, marginTop: 10 },
-  sliderOptions: { flexDirection: 'row', alignItems: 'center' },
-  sliderValue: { marginLeft: 26, fontFamily: 'Oswald', fontWeight: '700', fontSize: 18, color: '#2B2B2B' },
-  sliderIcon: { width: 29, height: 28, marginLeft: 23 },
   block: {
     height: 118,
     width: '100%',
@@ -242,11 +307,31 @@ const styles = StyleSheet.create({
 
     elevation: 3,
   },
-  writeFieldTitle: { fontWeight: '800', fontSize: 12, color: '#00293B', position: 'absolute', backgroundColor: '#fff', top: 5, left: 25, zIndex: 1 },
-  writeInput: { borderWidth: 1, borderColor: 'rgba(0, 0, 0, 0.32)', borderRadius: 4, height: 86, padding: 11, paddingTop: 11, fontWeight: '400', fontSize: 16, color: '#00293B' },
-  textWrap: { flexDirection: 'row', justifyContent: 'space-between' },
-  optionTitle: { fontWeight: '500', fontSize: 14, paddingBottom: 8, paddingLeft: 3 },
-  optionValue: { fontWeight: '700', fontSize: 18 },
-  gradientLine: { width: '100%', height: 6, borderRadius: 3, alignItems: 'flex-end', marginBottom: 16 },
-  grayLine: { backgroundColor: '#F4F4FB', height: 6, }
+  tab: { color: '#7D86A9', fontFamily: 'Oswald', fontWeight: '700', fontSize: 20, fontFamily: 'Oswald', textTransform: 'uppercase' },
+  activeTab: { color: '#5EC422', fontFamily: 'Oswald', fontWeight: '700', fontSize: 20, fontFamily: 'Oswald', textTransform: 'uppercase' },
+  tabs: {
+    top: -32,
+    height: 64, borderRadius: 32, backgroundColor: '#FFFF', width: '100%', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 3.22,
+    elevation: 3,
+  },
+  nameWrap: { height: 113, width: '100%', backgroundColor: 'rgba(0,0,0,0.5)', position: 'absolute', bottom: 0, paddingLeft: 14, paddingTop: 15, flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20 },
+  name: { fontWeight: '700', fontSize: 32, color: '#FFF', width: 290 },
+  options: { fontWeight: '500', fontSize: 12, color: '#FFF' },
+  logo1: { width: 42, height: 42, borderRadius: 21 },
+  logo2: { width: 42, height: 42, marginTop: 9 },
+  avarageValue: { fontFamily: 'Oswald', fontSize: 32, fontWeight: '700', marginTop: 7, color: '#FFFFFF', textAlign: 'center' },
+  image: { height: 315, width: '100%' },
+  bodyHeader: { height: 315, width: '100%' },
+  wrapCarousel2: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+  arrowIcon: { width: 26, height: 26 },
+  carousel: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 34, marginBottom: 26 },
+  avarageShowWrap: { width: 280, height: 20, alignItems: 'center' },
+  avarageShowText: { fontFamily: 'Oswald', fontweight: '700', fontSize: 16, textTransform: 'uppercase', color: '#00293B' },
+  arrowIcon2: { width: 6.72, height: 11.26 },
 })
